7e59bc5ff2fde6293f7de6c91e1256ac5e798aea
rian-124 <afriyanlionel0@gmail.com>
2024-12-20 20:01:07 +0700

    add side navbar

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 const signNav = document.querySelector('.sign-nav') ;
 const navbarNav = document.querySelector('.navbar-nav');
 const extraNavbar = document.querySelector('.extra-navbar');
-// const extraNavbar = document.querySelector('.extra-navbar');
 
 function moveSignNav() {
     
@@ -24,6 +23,14 @@ document.querySelector('#humberger-menu').onclick = () => {
     navbarNav.classList.toggle('active')
 
 } 
+
+const humberger = document.querySelector('#humberger-menu');
+
+document.addEventListener('click', (e) => {
+    if (!navbarNav.contains(e.target) && !humberger.contains(e.target) && !signNav.contains(e.target)) {
+        navbarNav.classList.remove('active');
+    }
+});
 
 
 window.addEventListener('resize', moveSignNav);
